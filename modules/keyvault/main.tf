65e9c6e4c70f4edee2c757224129298e53b8dfc8
jcolarte <48626017+jcolarte@users.noreply.github.com>
2026-09-25 16:42:48 -0500

    key vault

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,5 +1,17 @@
 data "azurerm_client_config" "current" {}
 
+resource "azurerm_key_vault" "kv" {
+  name                        = var.keyvault_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  enabled_for_disk_encryption = true
+  purge_protection_enabled    = var.environment == "prod" ? true : false
+  soft_delete_retention_days  = 7
+  tags                        = { Environment = var.environment }
+}
+
 resource "azurerm_key_vault_access_policy" "current_caller" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
